b908fdffdff080eec5851406bbb86758dd62f2e3
JoeyGinorio <joeyvelez@knights.ucf.edu>
2017-06-02 17:55:06 -0400

    Can now retrieve current state of MDP using getTerms()

--- src/OOMDP.py
+++ src/OOMDP.py
@@ -3,82 +3,144 @@
 # OOMDP Implementation
 # --------------------
 
+import itertools
 import numpy as np
 from Object import Object
 
 class OOMDP(object):
 	"""
 		Defines an Object-Oriented Markov Decision Process, formally defined 
-		as an eight tuple, < c, att(c), dom(a), o, a, t, r, lam >, where:
+		as a nine tuple, 
+		< classes, attributes(c), domain(a), objects, relations, actions, transitions,
+		rewards, discount >, where:
 
-			c -> is a set of object classes
+			classes -> is a set of object classes.
 
-			att(c) -> is a function that specifies attributes of a class c
+			attributes(c) -> is a function that specifies attributes of a 
+				class c.
 
-			dom(a) -> is a function that specifies the range of possible values for
-				an attribute a
+			domain(a) -> is a function that specifies the range of possible 
+				values for an attribute a.
 
-			o -> is a collection of objects, each of which belonds to a class, C. 
-				The state of an object is a value assignment to all its attributes.
+			objects -> is a set of objects, each of which belongs to a 
+				class, C. The state of an object is a value assignment to all 
+				its attributes.
 
-			a -> is a finite set of actions
+			relations -> is a set of relations, specifying how two objects may
+				interact. 
 
-			t -> specifies T(s'|s,a), probability an agent goes to s' by taking 
-				action a in state s. 
+			actions -> is a finite set of actions.
 
-			r -> specifies R(s,a), the reward an agent receives from the environment
-				for taking action a in state s.
+			transitions -> specifies T(s'|s,a), probability an agent goes to s'
+				by taking action a in state s. 
 
-			lam -> is the discount factor, [0,1). The smaller lambda is, the more 
-				the agent will favor immediate reward.
+			rewards -> specifies R(s,a), the reward an agent receives from the 
+				environment for taking action a in state s.
+
+			discount -> is the discount factor, [0,1). The smaller lambda is, 
+				the more the agent will favor immediate reward.
 	"""
 
-	def __init__(self, o=None, a=None, t=None, r=None, lam=None):
+	def __init__(self, objects=None, relations=None, actions=None, 
+				transitions=None, rewards=None, discount=None):
 		"""
-			Initializes parts of the eight tuple (a, t, r, lam), then
+			Initializes parts of the nine tuple (a, t, r, lam), then
 			extracts c, att(c), and dom(a) from o for the fully-defined
 			OOMDP. 
 
 			Params:
-				o -> is a 3D dict mapping objects to classes to attributes.
-					{Object: {Class: {Attribute: Value}}}
+				objects -> is a 3D dict mapping objects to classes to 
+				attributes.	{Object: {Class: {Attribute: Value}}}
 
-				a -> is a list of actions, e.g. [Up, Down, Left, Right]
+				relations -> is a 2D dict mapping a set of classes, (c_1, c_2),
+					to a list of boolean functions that operate over the set
+					of classes.
 
-				t -> is a 3D Matrix, specifiyng transition dynamics from state
-					to state, T[s'|s,a]. This can be learned through DOORMAX.
+				actions -> is a list of actions, e.g. [Up, Down, Left, Right]
 
-				r -> is a TBD
+				transitions -> is a 3D Matrix, specifiyng transition dynamics 
+					from state to state, T[s'|s,a]. This can be learned through
+					DOORMAX.
+
+				rewards -> is a TBD
 				
-				lam -> is a discrete value [0,1), the lower it is the more 
+				discount -> is a discrete value [0,1), the lower it is the more 
 					the agent prefers immediate reward.
 		"""
 
-		# Initialize part of the eight-tuple OOMDP, M
-		self.o = o
-		self.a = a
-		self.t = t
-		self.r = r
-		self.lam = lam
+		# Initialize part of the nine-tuple OOMDP, M
+		self.relations = relations
+		self.actions = actions
+		self.transitions = transitions
+		self.rewards = rewards
+		self.discount = discount
 
 		# Instantiate all instances of Object from 3D dict, O.
-		objects = list()
-		for obj in o:
+		updatedObjects = list()
+		for obj in objects:
 
-			# Extract object namef
+			# Extract object name
 			temp = obj
 
 			# Use name to create instance of Object, initialize with 
 			# 	class membership, and attributes with values
-			temp = Object(temp, o[temp].keys()[0], o[temp].values()[0])
-			objects.append(temp)
+			temp = Object(temp, objects[temp].keys()[0], objects[temp].values()[0])
+			updatedObjects.append(temp)
 
 		# O is now in a more accessible representation
-		self.o = objects
-
-		
+		self.objects = updatedObjects
 
 
+	def getTerms(self):
+		""" 
+			Retrieves all terms in the OOMDP. Terms come from all boolean 
+			functions, which can be from object attributes, relations defined,
+			over classes, and any boolean function defined over the state space
+			that encodes prior knowledge.
+		"""
+
+		terms = ""
+
+		# Extract terms from objects
+		for o in self.objects:
+			terms += o.state()
+
+		# Extract terms from relations
+		for key, val in self.relations.items():
+			terms += self.checkRelations({key:val})
+
+		# Extract terms from boolean functions encoding prior knowledge
+
+
+		return terms
+
+	def checkRelations(self, relation):
+		"""
+			Provided a list of boolean functions defined over classes, returns
+			a string of terms corresponding to the T/F value of each relation.
+		"""
+		# Figure out which classes the relations are defined over
+		classPair = list(relation.keys()[0])
+
+		# Collect all objects of each class
+		o1 = list()
+		o2 = list()
+		for o in self.objects:
+			if o.c == classPair[0]:
+				o1.append(o)
+			if o.c == classPair[1]:
+				o2.append(o)
+
+		# Construct all object pairs that will be tested on the relation
+		objectPairs = list(itertools.product(o1,o2))
+
+		# Test all object pairs on all relations, return string of terms
+		terms = ""
+		for objPair in objectPairs:
+			for rel in relation.values()[0]:
+				terms += str(int(rel(objPair[0], objPair[1])))
+
+		return terms
 
 
 
